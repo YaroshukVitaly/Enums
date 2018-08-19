ede3a67201ebe8f8acc27720c70615b813e30fa8
Vitalya <yaroshukvi@gmail.com>
2018-08-19 21:11:27 +0300

    + SWITCH

--- src/by/yaroshuk/Enums.java
+++ src/by/yaroshuk/Enums.java
@@ -10,8 +10,21 @@ public class Enums {
 
     public static void main(String[] args) {
         Season s = Season.AUTUMM;
-        System.out.println(s);
-        System.out.println(Arrays.toString(Season.values()));
+
+        switch (s) {
+            case WINTER:
+                System.out.println("Too cold!");
+                break;
+            case SPRING:
+                System.out.println("Good!");
+                break;
+            case SUMMER:
+                System.out.println("Hot!");
+                break;
+            case AUTUMM:
+                System.out.println("Good?");
+                break;
+        }
 
     }
 }
